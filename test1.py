a7be376900b6c8b829307c1632082e56160c4c71
edmondkying <56213297+edmondkying@users.noreply.github.com>
2019-11-28 00:26:41 -0500

    Add files via upload
    Use ResNet50 image network to identify object while vector is walking around
    It will display the image on its screen
    it will say out loud what it is
    It will print out what it is on the console

---
+++ test1.py
@@ -0,0 +1,49 @@
+
+from PIL import Image
+import numpy as np
+import anki_vector
+from anki_vector.events import Events
+from anki_vector.util import degrees
+import time
+from keras.applications.resnet50 import ResNet50
+from keras.preprocessing import image
+from keras.applications.resnet50 import preprocess_input, decode_predictions
+model = ResNet50(weights='imagenet')
+
+
+with anki_vector.Robot() as robot:
+
+    # If necessary, move Vector's Head and Lift down
+    screen_dimensions = anki_vector.screen.SCREEN_WIDTH, anki_vector.screen.SCREEN_HEIGHT
+
+    while True:
+        robot.camera.init_camera_feed()
+        robot.vision.enable_display_camera_feed_on_face(True)
+        
+        aimage = robot.camera.latest_image.raw_image.save('./latest.jpg', 'JPEG')
+        oimage = Image.open('./latest.jpg')
+        print("Display image on Vector's face...")
+        screen_data = anki_vector.screen.convert_image_to_screen_data(oimage.resize(screen_dimensions))
+        robot.screen.set_screen_with_image_data(screen_data, 5.0, True)
+        
+        robot.vision.enable_display_camera_feed_on_face(False)
+        robot.camera.close_camera_feed()
+        
+        img = image.load_img('./latest.jpg', target_size=(224,224))
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        x = preprocess_input(x)
+
+        preds = model.predict(x)
+
+        word = decode_predictions(preds, top=1)
+        for i in (word):
+            for j in i:
+                obj = j[1]
+        obj = obj.replace("_"," ")
+        print('Predicted:', obj)
+
+        robot.behavior.say_text('Might be {}'.format(obj))
+        robot.disconnect()
+        time.sleep(15)
+        robot.connect()
